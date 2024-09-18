f855e4fa2b0c103b33b8755c5fdaa68dcdcb95b0
sasa2754 <sasamor5@gmail.com>
2024-09-18 11:10:12 -0300

    competencias

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const sequelize = require('sequelize');
 //configurações da base de dados
 const database = new sequelize('ProvaWeb', 'ProvaWeb', 'etsps2024401', 
 {
-    dialect: 'mssql', host:'localhost', port: 54317
+    dialect: 'mssql', host:'localhost', port: 1433
 });
 
 database.sync();
